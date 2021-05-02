4c71dbccfa21d9c375e890dc7b0f53cdba2d6b60
bandarr009 <81906738+bandarr009@users.noreply.github.com>
2021-05-02 10:37:09 +0300

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -11455,9 +11455,9 @@ if text == "تفعيل الافلام" and Mod(msg) then
 send(msg.chat_id_, msg.id_,'• تم تفعيل الافلام')
 database:set(bot_id.."SOFI:movie_bot"..msg.chat_id_,"open")
 end
-if text and text:match("^فلم (.*)$") and database:get(bot_id.."SOFI:movie_bot"..msg.chat_id_) == "open" then
+if text and text:match("^فلم (.*)$") and database:get(bot_id.."FUAK:movie_bot"..msg.chat_id_) == "open" then
 local Textm = text:match("^فلم (.*)$")
-data,res = https.request('https://forhassan.ml/Black/movie.php?serch='..URL.escape(Textm)..'')
+data,res = https.request('https://black-source.tk/BlackTeAM/movie.php?serch='..URL.escape(Textm)..'')
 if res == 200 then
 getmo = json:decode(data)
 if getmo.Info == true then
