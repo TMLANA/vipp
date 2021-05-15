dd53a601c7354f02b2e1de86fd003792aed5f9ae
bandarr009 <81906738+bandarr009@users.noreply.github.com>
2021-05-15 08:24:08 +0300

    Add files via upload

--- Script.lua
+++ Script.lua
@@ -1447,12 +1447,17 @@ end
 return false
 end 
 
-if MsgText[1] == "الرابط" then
-if not redis:get(boss.."lock_linkk"..msg.chat_id_) then return "📡*¦* الامر معطل من قبل الادارة \n^"  end
-if not redis:get(boss..'linkGroup'..msg.chat_id_) then return "📡*¦* اوه 🙀 لا يوجد رابط ☹️\n🔖*¦*لانشاء رابط ارسل { انشاء رابط } \n📡" end
+if (MsgText[1] == "الرابط") then
+if not redis:get(boss.."lock_linkk"..msg.chat_id_) then 
+return "• الامر معطل من قبل الادارة \n^"  end
+if not redis:get(boss..'linkGroup'..msg.chat_id_) then 
+return "• لا يوجد رابط \n•لانشاء رابط ارسل 〚  انشاء رابط  〛 \n" 
+end
 local GroupName = redis:get(boss..'group:name'..msg.chat_id_)
 local GroupLink = redis:get(boss..'linkGroup'..msg.chat_id_)
-return "["..GroupName.."]("..GroupLink..")\n"
+local LinkG = "["..GroupName.."]("..GroupLink..")"
+return
+sendMsg(msg.chat_id_,msg.id_,LinkG)
 end
 
 if MsgText[1] == "ضع القوانين" then
