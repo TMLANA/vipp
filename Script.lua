d2d4adab35c7fd40b1655b4aec1299ff376e56de
bandarr009 <81906738+bandarr009@users.noreply.github.com>
2021-05-15 07:29:01 +0300

    Add files via upload

--- Script.lua
+++ Script.lua
@@ -1448,15 +1448,15 @@ return false
 end 
 
 if (MsgText[1] == "الرابط") then
-if not redis:get(amrko.."lock_linkk"..msg.chat_id_) then 
+if not redis:get(boss.."lock_linkk"..msg.chat_id_) then 
 return "• الامر معطل من قبل الادارة \n^"  end
-if not redis:get(amrko..'linkGroup'..msg.chat_id_) then 
+if not redis:get(boss..'linkGroup'..msg.chat_id_) then 
 return "• لا يوجد رابط \n•لانشاء رابط ارسل 〚  انشاء رابط  〛 \n" 
 end
 local GroupName = redis:get(amrko..'group:name'..msg.chat_id_)
 local GroupLink = redis:get(amrko..'linkGroup'..msg.chat_id_)
 local LinkG = "["..GroupName.."]("..GroupLink..")"
-return
+return 
 sendMsg(msg.chat_id_,msg.id_,LinkG)
 end
 
